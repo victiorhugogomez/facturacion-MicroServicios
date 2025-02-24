e7600b59e7e2b5640ce000da0053cb465549b05b
victor hugo gomez soto <victorhugo.gomezsoto@gmail.com>
2025-02-24 10:03:51 -0600

    primer commit

---
+++ facturacion-service/src/main/java/com/facturacionv/facturacion/FacturacionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.facturacionv.facturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionServiceApplication.class, args);
+	}
+
+}
---
+++ facturacion-service/src/test/java/com/facturacionv/facturacion/FacturacionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facturacionv.facturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
